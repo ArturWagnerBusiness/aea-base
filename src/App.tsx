418dcde5a92fe036768d3008fa484dca523deaa9
ArturWagnerBusiness <35738771+ArturWagnerBusiness@users.noreply.github.com>
2023-09-01 08:44:05 -0400

    Boilerplate finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const theme = createTheme({
+  palette: {
+    mode: "light", // "dark"
+  },
+});
+
+export default class App extends React.Component{
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <p>This should be visible on the App</p>
+        </div>
+      </ThemeProvider>
+    );
+  }
 }
-
-export default App;
